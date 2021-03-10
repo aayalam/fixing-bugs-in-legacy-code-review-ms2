e1df39408b5fdcbfa6cfd8b0c69078419fae4dee
Arnaldo Ayala <ayala@consultoresinformaticos.com.py>
2021-03-10 13:27:19 -0300

    Completed milestone

--- HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTests.java
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTests.java
@@ -12,12 +12,14 @@ import static com.assetco.search.results.HotspotKey.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BugsTests {
+
+    private SearchResults searchResults;
+
     @Test
     public void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
-      final var partnerVendor = makeVendor(Partner);
-      final int maximumShowcaseItems = 5;
-        var missing =
-          givenAssetInResultsWithVendor(partnerVendor);
+        final var partnerVendor = makeVendor(Partner);
+        final int maximumShowcaseItems = 5;
+        var missing = givenAssetInResultsWithVendor(partnerVendor);
         givenAssetInResultsWithVendor(makeVendor(Partner));
         var expected = givenAssetsInResultsWithVendor(maximumShowcaseItems - 1, partnerVendor);
 
@@ -28,22 +30,36 @@ public class BugsTests {
     }
 
     private AssetVendor makeVendor(AssetVendorRelationshipLevel relationshipLevel) {
-      return null;
+        return new AssetVendor("id", "displayName", relationshipLevel, 0);
     }
 
     private Asset givenAssetInResultsWithVendor(AssetVendor vendor) {
-        return null;
+        AssetPurchaseInfo purchaseInfoLast30Days = new AssetPurchaseInfo(0, 0,
+                new Money(BigDecimal.valueOf(0)), new Money(BigDecimal.valueOf(0)));
+        AssetPurchaseInfo purchaseInfoLast24Hours = new AssetPurchaseInfo(0, 0,
+                new Money(BigDecimal.valueOf(0)), new Money(BigDecimal.valueOf(0)));
+
+        Asset asset = new Asset("id", "title", null, null, purchaseInfoLast30Days, purchaseInfoLast24Hours,
+                null, vendor);
+        searchResults.addFound(asset);
+        return asset;
     }
 
     private Asset getAsset(AssetVendor vendor) {
-      return null;
+        throw new OptimizerTestException();
     }
 
     private AssetPurchaseInfo getPurchaseInfo() {
-      return null;
+        throw new OptimizerTestException();
     }
 
     private void thenHotspotHasExactly(HotspotKey hotspotKey, List<Asset> expected) {
+
+        Asset[] membersAssets = searchResults.getHotspot(hotspotKey).getMembers().toArray(new Asset[0]);
+        Asset[] expectedAssets = expected.toArray(new Asset[0]);
+
+        assertArrayEquals(membersAssets, expectedAssets);
+
     }
 
     private ArrayList<Asset> givenAssetsInResultsWithVendor(int count, AssetVendor vendor) {
@@ -55,8 +71,30 @@ public class BugsTests {
     }
 
     private void whenOptimize() {
+
+        SearchResultHotspotOptimizer optimizer = new SearchResultHotspotOptimizer();
+
+        optimizer.optimize(searchResults);
+
+
     }
 
     private void thenHotspotDoesNotHave(HotspotKey key, Asset... forbidden) {
+
+        searchResults.getHotspot(key).getMembers().forEach(asset -> {
+
+            Optional<Asset> searchedAsset = Arrays.stream(forbidden)
+                    .filter(forbiddenAsset -> forbiddenAsset.equals(asset))
+                    .findAny();
+
+            assertTrue(searchedAsset.isEmpty());
+
+        });
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        searchResults = new SearchResults();
     }
 }
---
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/OptimizerTestException.java
@@ -0,0 +1,10 @@
+package com.assetco.hotspots.optimization;
+
+public class OptimizerTestException extends RuntimeException {
+    public OptimizerTestException(String message) {
+        super(message);
+    }
+
+    public OptimizerTestException() {
+    }
+}
